9f6b918572a8dd9189080a4f5faf7dd98ed36371
Armen <armendje91@hotmail.com>
2012-04-01 20:31:48 -0430

    primera prueba con phyton

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: ISO-8859-1 -*-
+
+"""
+Python Project:
+
+"""
+
+#PLEASE EDIT build.xml IF YOU CHANGE THE NAME OF YOUR "main" SCRIPT.
+
+
+def main() :
+    """runs the whole project."""
+    print 'hola mundo';
+    pass
+    
+if __name__ == "__main__" :
+    main()
